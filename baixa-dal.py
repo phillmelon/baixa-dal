8604e17202b2be7f19f99b530671051f1c75cb94
FGomes <fgomes@al.sp.gov.br>
2022-04-16 01:26:13 -0300

    limpeza

--- baixa-dal.py
+++ baixa-dal.py
@@ -8,13 +8,14 @@ import argparse
 
 #Data da edicao de hoje
 AGORA = datetime.datetime.now()
+print(AGORA)
 ANO=str(AGORA.year)
-#MESES=["Janeiro","Fevereiro","MarÃ§o","Abril","Maio","Junho","Julho","Agosto","Setembro","Outubro","Novembro","Dezembro"]
+#MESES=["Janeiro","Fevereiro","Março","Abril","Maio","Junho","Julho","Agosto","Setembro","Outubro","Novembro","Dezembro"]
 MESES=["Janeiro","Fevereiro","Mar%C3%A7o","Abril","Maio","Junho","Julho","Agosto","Setembro","Outubro","Novembro","Dezembro"]
 MES=AGORA.month
 DIA=str(AGORA.day)
 
-#Informar a pagina inicial e final mÃ¡ximos
+#Informar a pagina inicial e final máximos
 PG1=1
 PG2=9999
 
@@ -66,12 +67,12 @@ def baixaPaginas(ano, mes, dia):
     print "Feito."
 
 ###################################
-# Cria um diretÃ³rio para o mÃªs.   #
+# Cria um diretório para o mês.   #
 # Junta as paginas em um arquivo. #
 # Apaga os pdfs originais.        #
 ###################################
 def juntaOsArquivos(ano, mes, dia):
-  #Cria o diretÃ³rio para o mÃªs
+  #Cria o diretório para o mês
   dirmes = os.getcwd() + os.sep + ano + "." + str(mes).zfill(2) + os.sep
   if not os.path.exists(dirmes):
     os.makedirs(dirmes)
@@ -105,10 +106,10 @@ def main():
   dia = DIA
 
   #Processa os argumentos
-  parser = argparse.ArgumentParser(prog="Baixa DAL",usage='Informe o ano, mÃªs e dia desejado no formato "AAAA MM DD", ou deixe em branco para baixar o DOE de hoje.',description='Baixa o DiÃ¡rio da Assembleia Legislativa de SP.')
-  parser.add_argument('ano', type=int, choices=range(int(ANO)-1, 2051), nargs='?', help='Informe o ano com 4 digitos. Este programa consegue recuperar ediÃ§Ãµes de um espaÃ§o da Imprensa Oficial onde ficam guardados as ediÃ§Ãµes dos Ãºltimos 5 ou 6 meses. Ã“bviamente este programa nÃ£o funciona para obter ediÃ§Ãµes de datas futuras :-p')
-  parser.add_argument('mes', type=int, choices=range(1, 13), nargs='?', help='O mÃªs desejado com 1 ou 2 digitos.')
-  parser.add_argument('dia', type=int, choices=range(1, 32), nargs='?', help='O dia do mÃªs, com 1 ou 2 digitos.')
+  parser = argparse.ArgumentParser(prog="Baixa DAL",usage='Informe o ano, mês e dia desejado no formato "AAAA MM DD", ou deixe em branco para baixar o DOE de hoje.',description='Baixa o Diário da Assembleia Legislativa de SP.')
+  parser.add_argument('ano', type=int, choices=range(int(ANO)-1, 2051), nargs='?', help='Informe o ano com 4 digitos. Este programa consegue recuperar edições de um espaço da Imprensa Oficial onde ficam guardados as edições dos últimos 5 ou 6 meses. Óbviamente este programa não funciona para obter edições de datas futuras :-p')
+  parser.add_argument('mes', type=int, choices=range(1, 13), nargs='?', help='O mês desejado com 1 ou 2 digitos.')
+  parser.add_argument('dia', type=int, choices=range(1, 32), nargs='?', help='O dia do mês, com 1 ou 2 digitos.')
   args = parser.parse_args()
   if args.ano:
     ano = str(args.ano)
@@ -124,8 +125,8 @@ def main():
   for f in files:
     os.remove(f)
 
-  print("Baixando o DiÃ¡rio do Legislativo do Estado de SP")
-  print("Data da ediÃ§Ã£o: " + ano + str(mes).zfill(2) + str(dia).zfill(2) + " (AAAAMMDD)")
+  print("Baixando o Diário do Legislativo do Estado de SP")
+  print("Data da edição: " + ano + str(mes).zfill(2) + str(dia).zfill(2) + " (AAAAMMDD)")
 
   baixaPaginas(ano, mes, dia)
   juntaOsArquivos(ano, mes, dia)
